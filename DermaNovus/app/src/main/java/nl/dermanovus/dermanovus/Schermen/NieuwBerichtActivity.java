dd4b3c3ba4715c0ec3f8bcef2d38cda0df9d5dab
nino-vrijman <n.vrijman@student.fontys.nl>
2016-01-14 16:52:03 +0100

    rbefore rebase

--- DermaNovus/app/src/main/java/nl/dermanovus/dermanovus/Schermen/NieuwBerichtActivity.java
+++ DermaNovus/app/src/main/java/nl/dermanovus/dermanovus/Schermen/NieuwBerichtActivity.java
@@ -1,18 +1,54 @@
 package nl.dermanovus.dermanovus.Schermen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.google.gson.Gson;
 
 import nl.dermanovus.dermanovus.R;
 
 public class NieuwBerichtActivity extends AppCompatActivity {
+    private ImageView iv1;
+    private ImageView iv2;
+    private ImageView iv3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nieuw_bericht);
+
+        iv1 = (ImageView)findViewById(R.id.imageView);
+        iv2 = (ImageView)findViewById(R.id.imageView2);
+        iv3 = (ImageView)findViewById(R.id.imageView3);
+
+        System.out.println("____On create called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("_____On resume called");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("NIEUWBERICHTACTIVITY", MODE_PRIVATE);
+
+        Gson gson = new Gson();
+        String fotoJSON;
+        Bitmap foto;
+
+        fotoJSON = sharedPreferences.getString("Foto", "");
+        if (!fotoJSON.isEmpty()) {
+            System.out.println("STRING NOT EMPTY");
+            foto = gson.fromJson(fotoJSON, Bitmap.class);
+            iv1.setImageBitmap(foto);
+        } else {
+            System.out.println("EMPTY");
+        }
     }
 
     public void btnHome_Click(View view) {
@@ -20,7 +56,8 @@ public class NieuwBerichtActivity extends AppCompatActivity {
     }
 
     public void btnVoegFotosToe_Click(View view) {
-        startActivity(new Intent(this, DermaCamActivity.class));
+        Intent fotoIntent = new Intent(this, DermaCamActivity.class);
+        startActivity(fotoIntent);
     }
 
     public void btnPlaatsReactie_Click(View view) {
--- DermaNovus/app/src/main/java/nl/dermanovus/dermanovus/Schermen/PreviewScherm.java
+++ DermaNovus/app/src/main/java/nl/dermanovus/dermanovus/Schermen/PreviewScherm.java
@@ -1,9 +1,18 @@
 package nl.dermanovus.dermanovus.Schermen;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.google.gson.Gson;
 
 import nl.dermanovus.dermanovus.R;
 
@@ -13,9 +22,27 @@ public class PreviewScherm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preview_scherm);
+
+        SharedPreferences sharedPref = getSharedPreferences("Foto1", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String fotoStringJSON = sharedPref.getString("Foto1", "");
+        Bitmap gemaakteFoto = gson.fromJson(fotoStringJSON, Bitmap.class);
+
+        ImageView gemaakteFotoView = (ImageView)findViewById(R.id.ivPreview);
+        gemaakteFotoView.setImageBitmap(gemaakteFoto);
     }
 
     public void btnHome(View view) {
         startActivity(new Intent(this, DermaCamActivity.class));
+    }
+
+
+    public void btnKeurFotoGoed_Click(View view) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Test");
+        alert.setMessage("Wilt u een nieuwe foto maken?");
+        alert.setPositiveButton("Ja", null);
+        alert.setNegativeButton("Nee", null);
+        alert.show();
     }
 }
